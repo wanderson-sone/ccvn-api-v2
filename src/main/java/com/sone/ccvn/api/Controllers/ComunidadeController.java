1a1645d393356b0b946752dedc6a29f8b2f0b7c8
wanderson-sone <wandersonslisboa@gmail.com>
2021-05-01 22:29:08 -0300

    created: criação do controller comunidade

---
+++ src/main/java/com/sone/ccvn/api/Controllers/ComunidadeController.java
@@ -0,0 +1,14 @@
+package com.sone.ccvn.api.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/community")
+public class ComunidadeController {
+
+    @RequestMapping
+    public String getComunidade(){
+        return "Comunidade rest funcionando!...";
+    }
+}
