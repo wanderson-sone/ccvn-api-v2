d2e826baaa2f392d4991dba0620bbbe611d70f3d
wanderson-sone <wandersonslisboa@gmail.com>
2021-05-02 00:30:23 -0300

    updated: refatoracao da classe ComunidadeController e ComunidadeService

--- src/main/java/com/sone/ccvn/api/controllers/ComunidadeController.java
+++ src/main/java/com/sone/ccvn/api/controllers/ComunidadeController.java
@@ -1,11 +1,13 @@
 package com.sone.ccvn.api.controllers;
 
+import com.sone.ccvn.api.dtos.request.ComunidadeDTO;
 import com.sone.ccvn.api.dtos.response.MessageResponseDTO;
-import com.sone.ccvn.api.entities.Comunidade;
 import com.sone.ccvn.api.services.ComunidadeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/community")
@@ -20,7 +22,7 @@ public class ComunidadeController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO createComunidade(@RequestBody Comunidade comunidade) {
-        return service.createComunidade(comunidade);
+    public MessageResponseDTO createComunidade(@RequestBody @Valid ComunidadeDTO comunidadeDTO) {
+        return service.createComunidade(comunidadeDTO);
     }
 }
--- src/main/java/com/sone/ccvn/api/services/ComunidadeService.java
+++ src/main/java/com/sone/ccvn/api/services/ComunidadeService.java
@@ -1,5 +1,7 @@
 package com.sone.ccvn.api.services;
 
+import com.sone.ccvn.api.dtos.mapper.ComunidadeMapper;
+import com.sone.ccvn.api.dtos.request.ComunidadeDTO;
 import com.sone.ccvn.api.dtos.response.MessageResponseDTO;
 import com.sone.ccvn.api.entities.Comunidade;
 import com.sone.ccvn.api.repositories.ComunidadeRepository;
@@ -11,13 +13,16 @@ public class ComunidadeService {
 
     private ComunidadeRepository repo;
 
+    private  final ComunidadeMapper mapper = ComunidadeMapper.INSTANCE;
+
     @Autowired
     public ComunidadeService(ComunidadeRepository repo) {
         this.repo = repo;
     }
 
-    public MessageResponseDTO createComunidade(Comunidade comunidade) {
-        Comunidade savedComunidade = repo.save(comunidade);
+    public MessageResponseDTO createComunidade(ComunidadeDTO comunidadeDTO) {
+        Comunidade comunidadeToSave = mapper.toComunidade(comunidadeDTO);
+        Comunidade savedComunidade = repo.save(comunidadeToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created Comunidade with ID " + savedComunidade.getId())
