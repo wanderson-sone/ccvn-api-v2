22d3411d8e53a427dc5f52f8db6ee60504edb865
wanderson-sone <wandersonslisboa@gmail.com>
2021-05-01 22:42:31 -0300

    created: criação da entidade COMUNIDADE

---
+++ src/main/java/com/sone/ccvn/api/Entities/Comunidade.java
@@ -0,0 +1,48 @@
+package com.sone.ccvn.api.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comunidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(name = "razao_social", nullable = false)
+    private String razaoSocial;
+
+    @Column(nullable = false)
+    private String cnpj;
+
+    @Column(name = "data_criacao", nullable = false)
+    private Date dataCriacao;
+
+    @Column(name = "data_atualizacao", nullable = false)
+    private Date dataAtualizacao;
+
+    @PreUpdate
+    public void preUpdate() {
+        dataAtualizacao = new Date();
+    }
+
+    @PrePersist
+    public void prePersist() {
+        final Date atual = new Date();
+        dataCriacao = atual;
+        dataAtualizacao = atual;
+    }
+}
