13ca1abf4f81806b92ca1e26c7a36bafc6059099
wanderson-sone <wandersonslisboa@gmail.com>
2021-05-02 00:28:29 -0300

    updated: refatoracao da classe ComunidadeDTO e TelefoneDTO

--- src/main/java/com/sone/ccvn/api/dtos/request/ComunidadeDTO.java
+++ src/main/java/com/sone/ccvn/api/dtos/request/ComunidadeDTO.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CNPJ;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -15,11 +19,18 @@ public class ComunidadeDTO {
 
     private Long id;
 
+    @NotEmpty
+    @Size(min = 2, max = 100)
     private String nome;
 
+    @Size(min = 2, max = 100)
     private String razaoSocial;
 
+    @NotEmpty
+    @CNPJ
     private String cnpj;
 
+    @Valid
+    @NotEmpty
     private List<TelefoneDTO> telefones;
 }
--- src/main/java/com/sone/ccvn/api/dtos/request/TelefoneDTO.java
+++ src/main/java/com/sone/ccvn/api/dtos/request/TelefoneDTO.java
@@ -6,6 +6,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -14,7 +19,10 @@ public class TelefoneDTO {
 
     private Long id;
 
+    @Enumerated(EnumType.STRING)
     private TelefoneTipo tipo;
 
-    private String nome;
+    @NotEmpty
+    @Size(min = 13, max = 14)
+    private String numero;
 }
