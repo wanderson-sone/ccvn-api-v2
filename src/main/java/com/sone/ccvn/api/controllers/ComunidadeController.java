715e134dfc1d9feb8b53e0cf7923b7bd73f7be97
wanderson-sone <wandersonslisboa@gmail.com>
2021-05-01 23:54:09 -0300

    updated: refatoracao do classe ComunidadeController

--- src/main/java/com/sone/ccvn/api/controllers/ComunidadeController.java
+++ src/main/java/com/sone/ccvn/api/controllers/ComunidadeController.java
@@ -1,14 +1,27 @@
 package com.sone.ccvn.api.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sone.ccvn.api.dtos.response.MessageResponseDTO;
+import com.sone.ccvn.api.entities.Comunidade;
+import com.sone.ccvn.api.repositories.ComunidadeRepository;
+import com.sone.ccvn.api.services.ComunidadeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/community")
 public class ComunidadeController {
 
-    @RequestMapping
-    public String getComunidade(){
-        return "Comunidade rest funcionando!...";
+    private ComunidadeService service;
+
+    @Autowired
+    public ComunidadeController(ComunidadeService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createComunidade(@RequestBody Comunidade comunidade) {
+        return service.createComunidade(comunidade);
     }
 }
