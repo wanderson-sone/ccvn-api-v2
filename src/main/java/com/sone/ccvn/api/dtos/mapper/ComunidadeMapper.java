8009e4f5935ef9c7dbce3fd9e42e6a11c7a2f0a8
wanderson-sone <wandersonslisboa@gmail.com>
2021-05-03 21:49:17 -0300

    updated: ComunidadeMapper

--- src/main/java/com/sone/ccvn/api/dtos/mapper/ComunidadeMapper.java
+++ src/main/java/com/sone/ccvn/api/dtos/mapper/ComunidadeMapper.java
@@ -3,6 +3,7 @@ package com.sone.ccvn.api.dtos.mapper;
 import com.sone.ccvn.api.dtos.request.ComunidadeDTO;
 import com.sone.ccvn.api.entities.Comunidade;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -10,6 +11,7 @@ public interface ComunidadeMapper {
 
     ComunidadeMapper INSTANCE = Mappers.getMapper(ComunidadeMapper.class);
 
+    @Mapping(target = "dataFundacao", source = "dataFundacao", dateFormat = "dd-MM-yyyy")
     Comunidade toComunidade(ComunidadeDTO comunidadeDTO);
 
     ComunidadeDTO comunidadeToComunidadeDTO (Comunidade comunidade);
